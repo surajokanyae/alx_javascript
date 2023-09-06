ecf7dfe77fa272a76aad00964b1745aff0631788
sgkanya <sgkanyae@gmail.com>
2023-09-06 22:29:21 +0100

    Add and export integer

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,15 +1,3 @@
-function add(a, b) {
-    /*
-    This function returns the addition of two integers.
-  
-    Args:
-      a: The first integer.
-      b: The second integer.
-  
-    Returns:
-      The sum of a and b.
-    */
-  
-    return a + b;
-  }
-  
+exports.add = function (a, b) {
+    return (a + b);
+  };
