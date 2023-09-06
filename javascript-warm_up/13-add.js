44de13ad34d2c71be6f1ae15e9f8f8ca2d730fe0
sgkanya <sgkanyae@gmail.com>
2023-09-06 22:26:46 +0100

    Fuction to return integer

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    /*
+    This function returns the addition of two integers.
+  
+    Args:
+      a: The first integer.
+      b: The second integer.
+  
+    Returns:
+      The sum of a and b.
+    */
+  
+    return a + b;
+  }
+  
