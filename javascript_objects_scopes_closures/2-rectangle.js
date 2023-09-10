10042adae59cbe1eb998cf65dbb503bd1c7883d6
sgkanya <sgkanyae@gmail.com>
2023-09-10 22:01:31 +0100

    Rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        this.width = 0;
+        this.height = 0;
+      } else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  } 
+  module.exports = Rectangle
