0fbbfa0fafea47a2721ad82fe0f3729c608407b2
sgkanya <sgkanyae@gmail.com>
2023-09-10 22:08:23 +0100

    Rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        this.width = 0;
-        this.height = 0;
-      } else {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       }
