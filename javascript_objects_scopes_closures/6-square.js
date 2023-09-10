a82916aa4dc09bbb129632c76e092dcaf7bb2347
sgkanya <sgkanyae@gmail.com>
2023-09-10 23:05:16 +0100

    Square

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+class Square extends Square {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c = 'X'){
+    let row = '';
+    let i = 0;
+for(i = 0; i < this.width; i++){
+    row += 'X';
+}
+for(i = 0; i < this.heigh; i++){
+    console.log(row);
+}
+
+  }
+}
+module.exports = Square;
