2c8e0a283824a603100ba0c067a913b86c47203d
sgkanya <sgkanyae@gmail.com>
2023-09-10 22:26:30 +0100

    Rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
+    constructor (w, h) {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       }
     }
-   
-
-  print() {
-    let row = '';
-    let i = 0;
-    for (let i = 0; i < this.height; i++) {
+  
+    // Method to print the rectangle using 'X'
+    print () {
+      let row = '';
+      let i = 0;
+      for (i = 0; i < this.width; i++) {
         row += 'X';
-    }
-      for (let i = 0; i < this.width; i++) {
+      }
+      for (i = 0; i < this.height; i++) {
         console.log(row);
       }
     }
-}
+  }
   module.exports = Rectangle;
