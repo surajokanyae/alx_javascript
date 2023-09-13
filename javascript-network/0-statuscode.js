a87fca9ed362827af1be0a30b9e4771049e6d54e
sgkanya <sgkanyae@gmail.com>
2023-09-13 21:30:01 +0100

    StatusCode

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,14 +1,11 @@
+#!/usr/bin/node
+// script that displays the status code of a GET request
 const request = require('request');
-
-const getStatusCode = (url) => {
-  const response = request.get(url);
-  return response.statusCode;
-};
-
-const main = () => {
-  const url = process.argv[2];
-  const status = getStatusCode(url);
-  console.log(`code: ${status}`);
-};
-
-main();
+const myArgs = process.argv.slice(2);
+request(myArgs[0], function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
