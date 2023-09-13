7d9d6a4c4eb66b2daab16c99918f7c3c0f65cbbe
sgkanya <sgkanyae@gmail.com>
2023-09-13 21:34:43 +0100

    Title

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that displays the title of the Star Wars movie using API
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const URLstring = 'https://swapi-api.alx-tools.com/api/films/:id' + myArgs[0];
+request(URLstring, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
